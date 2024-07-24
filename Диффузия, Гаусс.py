096a26eb77e07b8b664d9f2d99252d65ff5c7993
SaveliySaltykov <ya.savva-saltykov@yandex.ru>
2024-07-24 15:39:44 +0300

    git add

--- Диффузия, Гаусс.py
+++ Диффузия, Гаусс.py
@@ -1,61 +1,66 @@
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
+'''В рамках борьбы с анархией сознания (ради порядка) начальные
+параметры вводятся в СГС, а расчёты ведутся в (пс,мкм,N0)'''
 Pi=3.141592653589
-Nx=31#количество узлов сетки
-Ny=31
-dx=2/(Nx-1)#сетка 2 на 2 мкм
-dy=2/(Ny-1)
+#ВВод начальных параметров, СГС
+Nx=41#количество узлов сетки
+Ny=41
+Lx=5*10**-4# см 
+Ly=5*10**-4# см 
+N_0=1*10**11# см^-2
+D_0=3*10**0# см^2/сек
+R_a=1*10**-1# см^2/сек
+V0D0_KbT=1*10**-10# см^4/сек
+tau=500*10**-12# сек
+R_0=0.4*10**-4# см
 #ДУ:z'=Az+Bz^2+C(лапласиан z)+D набла(z набла (z))
-#расстояние по сетке считается через мкм, время через пс
-#z=n/N0, где N0=5*10^10 см^-2
-#НУ:z(r,0)=E/Pi*Exp(-Fr^2)
-A=-2*10**-3#пс^-1
-B=-5*10**-3#пс^-1
-C=10**-4#мкм^2/пс
-D=5*10**-4#мкм^2/пс
-E=7.5
-F=6.25
+#z=n/N0, где n-концентрация
+#НУ:z(r,0)=Exp(-(r/r0)^2)
+#Вычисление вспомогательных параметров, пересчёт в (пс,мкм,N0)
+A=-1/tau*10**-12# пс^-1
+B=-1*R_a*N_0*10**-12# пс^-1
+C=D_0*10**-4#мкм^2/пс
+D=V0D0_KbT*N_0*10**-4#мкм^2/пс
+dx=Lx*10**4/(Nx-1)
+dy=Ly*10**4/(Ny-1)#Шаги сетки по осям Х и У, мкм
 t=0#время
-
-X, Y = np.meshgrid(
-    np.linspace(0, Nx-1, Nx),
-    np.linspace(0, Ny-1, Ny)
+dt=0.5#пс
+Y, X = np.meshgrid(
+    np.linspace(0, Ny-1, Ny),
+    np.linspace(0, Nx-1, Nx)
 )
-Z=E/Pi*np.exp(-F*((dx*(X-(Nx-1)/2))**2+(dy*(Y-(Ny-1)/2))**2))
-#получилось так, что элемент (x,y) определён как Z[y][x]
-fig,cs=plt.subplots()
-
-cs = plt.contourf(dx*(X-(Nx-1)/2),dy*(Y-(Ny-1)/2),Z*5*10**10,levels=15)
-cbar=plt.colorbar(cs)
-cbar.set_label('Концентрация N, см^-2')
-plt.title('Время t = '+str(t)+' пс')
-plt.xlabel('Ось X, мкм')
-plt.ylabel('Ось Y, мкм')
-
-def EulerStep(frame):
-    global Z,t
-    ZZ=0*X*Y
-    for k in range(10):#количество итераций за фрейм
-        for i in range(1,Ny-1):#будет цикл от 1 до Nx-2
-            for j in range(1,Nx-1):
-                N1=A*Z[i][j]+B*Z[i][j]*Z[i][j]
-                N2=C*((Z[i][j+1]-2*Z[i][j]+Z[i][j-1])/(dx*dx)+(Z[i+1][j]-2*Z[i][j]+Z[i-1][j])/(dy*dy))
-                N3=D*((Z[i][j+1]**2-2*Z[i][j]**2+Z[i][j-1]**2)/(2*dx*dx)+(Z[i+1][j]**2-2*Z[i][j]**2+Z[i-1][j]**2)/(2*dy*dy))
-                ZZ[i][j]=Z[i][j]+N1+N2+N3
-            
-        t=t+1
-    plt.clf()
-    Z=ZZ
+Z=np.exp(-1/R_0/R_0*10**-8*((dx*(X-(Nx-1)/2))**2+(dy*(Y-(Ny-1)/2))**2))#+10**-10
+#Элемент (x,y) определён как Z[x][y]
+'''Почему-то то, каким получится массив после умножения
+зависит только от положения осей X и Y внутри meshgrid.
+То есть Z=Y*X(=X*Y) даст такой же массив.'''
+def makeplot():
     cs = plt.contourf(dx*(X-(Nx-1)/2),dy*(Y-(Ny-1)/2),Z*5*10**10,levels=15)
     cbar=plt.colorbar(cs)
     cbar.set_label('Концентрация N, см^-2')
     plt.title('Время t = '+str(t)+' пс')
     plt.xlabel('Ось X, мкм')
-    plt.ylabel('Ось Y, мкм')    
-    if t==200:#Время (пс), на котором нужно остановить расчёт
+    plt.ylabel('Ось Y, мкм')
+def EulerStep(frame):
+    global Z,t
+    ZZ=0*X*Y
+    for k in range(10):#количество итераций за фрейм
+        for i in range(1,Nx-1):#будет цикл от 1 до Nx-2
+            for j in range(1,Ny-1):
+                N1=A*Z[i][j]+B*Z[i][j]*Z[i][j]
+                N2=C*((Z[i][j+1]-2*Z[i][j]+Z[i][j-1])/(dy*dy)+(Z[i+1][j]-2*Z[i][j]+Z[i-1][j])/(dx*dx))
+                N3=D*((Z[i][j+1]**2-2*Z[i][j]**2+Z[i][j-1]**2)/(2*dy*dy)+(Z[i+1][j]**2-2*Z[i][j]**2+Z[i-1][j]**2)/(2*dx*dx))
+                ZZ[i][j]=Z[i][j]+N1+N2+N3
+        t=t+1
+        Z=ZZ
+    plt.clf()
+    makeplot()   
+    if t==500:#Время (пс), на котором нужно остановить расчёт
         anim.event_source.stop()
-        
+fig,cs=plt.subplots()
+makeplot()
 anim=FuncAnimation(fig,EulerStep,frames=None)
 plt.show()
 
