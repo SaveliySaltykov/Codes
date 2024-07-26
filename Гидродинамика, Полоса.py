336e1a36dad868af872dabff15e97f7c8070bfec
SaveliySaltykov <ya.savva-saltykov@yandex.ru>
2024-07-26 14:42:08 +0300

    безумие ничего не работает!

--- Гидродинамика, Полоса.py
+++ Гидродинамика, Полоса.py
@@ -5,11 +5,10 @@ import matplotlib.pyplot as plt
 параметры вводятся в СГС, а расчёты ведутся в (пс,мкм,N0)'''
 Pi=3.141592653589
 #ВВод начальных параметров, СГС
-
-Nx=50
-Ny=50#Количество узлов сетки
+Nx=53#количество узлов сетки
+Ny=18
 Lx=5*10**-4# см 
-Ly=5*10**-4# см 
+Ly=1*10**-4# см 
 tau_0=5.3*10**-13# сек 
 KbT=5.52*10**-16# эрг
 m=0.62*9.1*10**-28# г
@@ -23,31 +22,33 @@ R_0=0.4*10**-4# см
 2)ур-ие для dVx/dt это муторный крокодил вида:
 Vx'=...-A(...)-B(...)-C(...)+D(...)
 3)ур-ие для Vy'''
-#Вычисление вспомогательных параметров, пересчёт в (пс,мкм,N0)
+#Вычисление вспомогательных параметров, пересчёт в (пс,мкм,?N0?возможно, надо отказаться от замены z=n/N0)
 A=1/tau_0*10**-12# пс^-1, 1сек=10^12пс
 B=KbT/m*10**-16#(мкм/пс)^2, 1см=10^4мкм
-C=V_0*N_0/m*10**-16# (мкм/пс)^2
+C=V_0/m*10**-8# (мкм^2/пс)^2
 D=tau_ex_ex*KbT/m*10**-4# мкм^2/пс
-dx=Lx*10**4/(Nx-1)
-dy=Ly*10**4/(Ny-1)#Шаги сетки по осям Х и У, мкм
+dx=Lx*10**4/(Nx-3)
+dy=Ly*10**4/(Ny-3)#Шаги сетки по осям Х и У, мкм
 t=0#время, пс
-dt=0.5#пс
+dt=1#пс
 Y, X = np.meshgrid(
     np.linspace(0, Ny-1, Ny),
     np.linspace(0, Nx-1, Nx)
 )
-Z=np.exp(-1/R_0/R_0*10**-8*((dx*(X-(Nx-1)/2))**2+(dy*(Y-(Ny-1)/2))**2))+10**-23
+Z=0*X*Y#+N_0*10**-8*10**-2#Костыль "фоновая засветка", поскольку концентрация в знаменателе
 #Элемент (x,y) определён как Z[x][y]
+for j in range(0,Ny):
+    Z[1][j]=N_0*10**-8#мкм^-2#НУ
+    Z[2][j]=N_0*10**-8
 Vx=0*X*Y#+dx*(X-(Nx-1)/2)/R_0*np.sqrt(KbT/m)*10**-12# мкм/пс
 #поле проекции скоростей Vx
 Vy=0*X*Y#+dy*(Y-(Ny-1)/2)/R_0*np.sqrt(KbT/m)*10**-12# мкм/пс
 #поле проекции скоростей Vy
-'''То, каким получится массив после умножения
+'''Почему-то то, каким получится массив после умножения
 зависит только от положения осей X и Y внутри meshgrid.
 То есть Z=Y*X(=X*Y) даст такой же массив.'''
-
 def D_x(N,x,y):#Взятие частной производной по x
-    return (N[x+1][y]-N[x-1][y])/(2*dx)
+    return (N[x+1][y]-N[x][y])/(dx)
 def D_y(N,x,y):#Взятие частной производной по y
     return (N[x][y+1]-N[x][y-1])/(2*dy)
 def Lap(N,x,y):#Взятие лапласиана
@@ -55,10 +56,26 @@ def Lap(N,x,y):#Взятие лапласиана
     N2=(N[x][y+1]-2*N[x][y]+N[x][y-1])/(dy*dy)
     return N1+N2
 def makeplot():
-    cs = plt.contourf(dx*(X-(Nx-1)/2),dy*(Y-(Ny-1)/2),Z*N_0,levels=15)
+    YY, XX = np.meshgrid(
+        np.linspace(0, Ny-3, Ny-2),
+        np.linspace(0, Nx-3, Nx-2)
+    )
+    ZZ=0*YY*XX
+    VVx=0*YY*XX
+    VVy=0*YY*XX
+    for i in range(0,Nx-2):
+        for j in range(0,Ny-2):
+            ZZ[i][j]=Z[i+1][j+1]
+    for i in range(0,Nx-2):
+        for j in range(0,Ny-2):
+            VVx[i][j]=Vx[i+1][j+1]
+    for i in range(0,Nx-2):
+        for j in range(0,Ny-2):
+            VVy[i][j]=Vy[i+1][j+1]
+    cs = plt.contourf(dx*XX,dy*YY,VVx,levels=15)#ZZ*10**8
     cbar=plt.colorbar(cs)
     cbar.set_label('Концентрация N, см^-2')
-    plt.title('Время t = '+str(t)+' пс (Гидродинамика)')
+    plt.title('Время t = '+str(t)+' пс')
     plt.xlabel('Ось X, мкм')
     plt.ylabel('Ось Y, мкм')
 def Eq1(x,y):
@@ -81,10 +98,20 @@ def Eq3(x,y):
     return (N1+N2+N3+N4+N5)/Z[x,y]
 def EulerStep(frame):
     global Z,Vx,Vy,t
+    plt.clf()
+    makeplot()
+    if round(t)==1000:#Время (пс), на котором нужно остановить расчёт
+        anim.event_source.stop()
     ZZ=0*X*Y
     VVx=0*X*Y
     VVy=0*X*Y
-    for k in range(20):#количество итераций за фрейм анимации
+    for j in range(0,Ny):
+        ZZ[0][j]=Z[1][j]
+        #VVx[0][j]=Vx[0][j]
+    for i in range(0,Nx):
+        ZZ[i][0]=Z[i][1]
+        ZZ[i][Ny-1]=Z[i][Ny-2]
+    for k in range(1):#количество итераций за фрейм анимации
         for i in range(1,Nx-1):#цикл от 1 до Nx-2
             for j in range(1,Ny-1):
                 ZZ[i][j]=Z[i][j]+Eq1(i,j)*dt#уравнение непрерывноести
@@ -97,31 +124,13 @@ def EulerStep(frame):
                 else:
                     VVx[i][j]=Vx[i][j]+Eq2(i,j)*dt#гидродинамическое
                     VVy[i][j]=Vy[i][j]+Eq3(i,j)*dt#гидродинамическое
-        for i in range(1,Nx-1):
-            VVy[i][0]=VVy[i][1]
-            VVy[i][Ny-1]=VVy[i][Ny-2]
-            VVx[i][0]=VVx[i][1]
-            VVx[i][Ny-1]=VVx[i][Ny-2]
-        for j in range(1,Nx-1):
-            VVx[0][j]=VVx[1][j]
-            VVx[Nx-1][j]=VVx[Nx-2][j]
-            VVy[0][j]=VVy[1][j]
-            VVy[Nx-1][j]=VVy[Nx-2][j]
-        Z=ZZ
         Vx=VVx
         Vy=VVy
-        t=t+dt
-    plt.clf()
-    makeplot()
-    
-    
-    if t==1000:#Время (пс), на котором нужно остановить расчёт
-        anim.event_source.stop()
+        t=round(t,2)+dt
+plt.rcParams ['figure.figsize'] = [30*Lx/(Lx+Ly), 30*Ly/(Lx+Ly)]
 fig,cs=plt.subplots()
 makeplot()
 anim=FuncAnimation(fig,EulerStep,frames=None)
-
 plt.show()
-print((np.sqrt(KbT/m))*10**-8)
 
 
